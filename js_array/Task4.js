1d4fdaf65e96264ab7356688af0eaf38f6521452
Isakawar <isakawar1@gmail.com>
2023-10-31 08:45:28 +0200

    Refactor Task4 with filter method

--- js_array/Task4.js
+++ js_array/Task4.js
@@ -1,14 +1,6 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let evenNumbers = []
-
-for (let i = numbers.length; i > 0;){
-    if (numbers[i] % 2 == 0){
-        evenNumbers.push(numbers[i])
-        i--
-    }
-    else{
-        i--
-    }
-}
+let evenNumbers = numbers.filter(function (number){
+    return number % 2 === 0
+})
 
 console.log(evenNumbers)
