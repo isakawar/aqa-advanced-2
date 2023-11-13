4e062d86fd97347f2411b214c00520e5e83c180d
Isakawar <isakawar1@gmail.com>
2023-11-13 12:40:11 +0200

    Add 'await' operation

--- js_promise/Taks3.js
+++ js_promise/Taks3.js
@@ -1,40 +1,43 @@
-const urlTodo = "https://jsonplaceholder.typicode.com/todos/1"
-const urlUser = "https://jsonplaceholder.typicode.com/users/1"
+const urlTodo = "https://jsonplaceholder.typicode.com/todos/1";
+const urlUser = "https://jsonplaceholder.typicode.com/users/1";
 
-function fetchTodo(){
-    return fetch(urlTodo)
-        .then((response) => response.json())
-        .then((todo) => todo)
-        .catch((error) => {
-            console.error('Error fetching todo:', error)
-            throw error
-        })
+async function fetchTodo() {
+  return await fetch(urlTodo)
+    .then((response) => response.json())
+    .then((todo) => todo)
+    .catch((error) => {
+      console.error("Error fetching todo:", error);
+      throw error;
+    });
 }
 
-function fetchUser() {
-    return fetch(urlUser)
-        .then(response => response.json())
-        .then(user => user)
-        .catch(error => {
-            console.error('Error fetching user:', error)
-        })
+async function fetchUser() {
+  return await fetch(urlUser)
+    .then((response) => response.json())
+    .then((user) => user)
+    .catch((error) => {
+      console.error("Error fetching user:", error);
+      throw error;
+    });
 }
-
 
 async function fetchData() {
-    try {
-        const [todoResult, userResult] = await Promise.all([fetchTodo(), fetchUser()])
+  try {
+    const [todoResult, userResult] = await Promise.all([
+      fetchTodo(),
+      fetchUser(),
+    ]);
 
-        console.log('Data from Promise.all:')
-        console.log('Todo:', todoResult)
-        console.log('User:', userResult)
+    console.log("Data from Promise.all:");
+    console.log("Todo:", todoResult);
+    console.log("User:", userResult);
 
-        const raceResult = await Promise.race([fetchTodo(), fetchUser()])
-        console.log('\nData from Promise.race:')
-        console.log(raceResult)
-    } catch (error) {
-        console.error('Error in fetchData:', error)
-    }
+    const raceResult = await Promise.race([fetchTodo(), fetchUser()]);
+    console.log("\nData from Promise.race:");
+    console.log(raceResult);
+  } catch (error) {
+    console.error("Error in fetchData:", error);
+  }
 }
 
-fetchData()
+fetchData();
