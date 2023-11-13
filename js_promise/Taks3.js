7eec8b8b0703b119da099193ce462fad837fe646
Isakawar <isakawar1@gmail.com>
2023-11-13 13:27:19 +0200

    refactore functions fetchUser and fetchTodo

--- js_promise/Taks3.js
+++ js_promise/Taks3.js
@@ -2,42 +2,28 @@ const urlTodo = "https://jsonplaceholder.typicode.com/todos/1";
 const urlUser = "https://jsonplaceholder.typicode.com/users/1";
 
 async function fetchTodo() {
-  return await fetch(urlTodo)
-    .then((response) => response.json())
-    .then((todo) => todo)
-    .catch((error) => {
-      console.error("Error fetching todo:", error);
-      throw error;
-    });
+  const response = await fetch(urlTodo);
+  return await response.json();
 }
 
 async function fetchUser() {
-  return await fetch(urlUser)
-    .then((response) => response.json())
-    .then((user) => user)
-    .catch((error) => {
-      console.error("Error fetching user:", error);
-      throw error;
-    });
+  const response = await fetch(urlUser);
+  return await response.json();
 }
 
 async function fetchData() {
-  try {
-    const [todoResult, userResult] = await Promise.all([
-      fetchTodo(),
-      fetchUser(),
-    ]);
+  const [todoResult, userResult] = await Promise.all([
+    fetchTodo(),
+    fetchUser(),
+  ]);
 
-    console.log("Data from Promise.all:");
-    console.log("Todo:", todoResult);
-    console.log("User:", userResult);
+  console.log("Data from Promise.all:");
+  console.log("Todo:", todoResult);
+  console.log("User:", userResult);
 
-    const raceResult = await Promise.race([fetchTodo(), fetchUser()]);
-    console.log("\nData from Promise.race:");
-    console.log(raceResult);
-  } catch (error) {
-    console.error("Error in fetchData:", error);
-  }
+  const raceResult = await Promise.race([fetchTodo(), fetchUser()]);
+  console.log("\nData from Promise.race:");
+  console.log(raceResult);
 }
 
 fetchData();
